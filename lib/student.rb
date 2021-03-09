1c77120068a2961ecab35d0c4a982ec3fb075ed3
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-09 12:13:37 -0500

    commit tree d339faff79564125aec86d8ffd8f71a0a0edbd3d

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
 
-end
+require_relative "./user.rb"
+
+class Student < User
+    attr_reader :knowledge
+    def initialize
+      @knowledge = []
+    end
+  
+    def new
+      @knowledge
+    end
+  
+    def learn(string_knowledge)
+      @knowledge << string_knowledge  
+    end
+
+end 
